a6c9115dc892c8eafd7a2062960f4d1aa0a71a21
Camilo <jcamilovelandiab@gmail.com>
2021-05-28 13:54:15 -0500

    Finishing the sidemenu component, and creating a form in the template/basics component

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface MenuItem {
+  text: string;
+  route: string;
+}
 
 @Component({
   selector: 'app-sidemenu',
@@ -6,11 +11,38 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class SidemenuComponent implements OnInit {
+export class SidemenuComponent {
+
+  templateMenu : MenuItem[] = [
+    {
+      text: 'Basics',
+      route: 'template/basics'
+    },
+    {
+      text: 'Dinamics',
+      route: 'template/dinamics'
+    },
+    {
+      text: 'Switches',
+      route: 'template/switches'
+    }
+  ];
+
+  reactiveMenu : MenuItem[] = [
+    {
+      text: 'Basics',
+      route: 'reactive/basics'
+    },
+    {
+      text: 'Dinamics',
+      route: 'reactive/dinamics'
+    },
+    {
+      text: 'Switches',
+      route: 'reactive/switches'
+    }
+  ];
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
