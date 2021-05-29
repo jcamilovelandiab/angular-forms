7f4eafe4d017221811cf1f8b477a525ba2d1b8fe
Camilo <jcamilovelandiab@gmail.com>
2021-05-29 16:18:55 -0500

    Finishing the reactive/switches component

--- src/app/reactive/pages/switches/switches.component.ts
+++ src/app/reactive/pages/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -8,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  myForm: FormGroup = this.formBuilder.group({
+    gender: [ 'M', Validators.required ],
+    notifications: [ true, Validators.required ],
+    conditions: [ false, Validators.requiredTrue ]
+  });
+
+  person = {
+    gender: 'F',
+    notifications: true
+  }
+
+  constructor( private formBuilder: FormBuilder ) { }
 
   ngOnInit(): void {
+    this.myForm.reset({
+      ...this.person,
+      conditions: false
+    });
+
+    this.myForm.valueChanges
+        .subscribe( ({ conditions, ...rest }) => {
+          this.person = rest;
+        });
+
+  }
+
+  save() {
   }
 
 }
