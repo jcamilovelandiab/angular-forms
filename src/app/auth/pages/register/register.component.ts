b2c6d13effbaeacc64d1974d2506b6aaf4d41498
Camilo <jcamilovelandiab@gmail.com>
2021-05-29 17:17:18 -0500

    Creating a custom validator

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -11,17 +11,31 @@ export class RegisterComponent implements OnInit {
 
   fullnamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
   emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+  
+  usernameValidator( formControl: FormControl) {
+    const value: string = formControl.value?.trim().toLowerCase();
+    if( value === 'camilo' ) {
+      return {
+        noCamilo: true
+      };
+    }
+    return null;
+  }
+
 
   registerForm: FormGroup = this.formBuilder.group({
     fullname: ['', [ Validators.required, Validators.pattern(this.fullnamePattern) ] ],
-    email: ['', [ Validators.required, Validators.pattern(this.emailPattern) ] ]
+    email: ['', [ Validators.required, Validators.pattern(this.emailPattern) ] ],
+    username: ['', [ Validators.required, this.usernameValidator ] ]
   });
 
   constructor( private formBuilder: FormBuilder ) { }
 
   ngOnInit(): void {
     this.registerForm.reset({
-      fullname: 'Camilo Velandia'
+      fullname: 'Camilo Velandia',
+      email: 'jcamilovelandiab@gmail.com',
+      username: 'jcamilovelandiab'
     });
   }
 
