e22ff7d730d0cc47748241e19fc1747e80d79ab5
Camilo <jcamilovelandiab@gmail.com>
2021-05-29 16:56:54 -0500

    Validating fullname field with a regex expression in the register component.

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  fullnamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+
+  registerForm: FormGroup = this.formBuilder.group({
+    fullname: ['', [ Validators.required, Validators.pattern(this.fullnamePattern) ] ]
+  });
+
+  constructor( private formBuilder: FormBuilder ) { }
 
   ngOnInit(): void {
+  }
+
+  validField( field: string ) {
+    return this.registerForm.get(field)?.invalid && 
+      this.registerForm.get(field)?.touched;
+  }
+
+  register() {
+    console.log(this.registerForm.value);
+    this.registerForm.markAllAsTouched();
   }
 
 }
